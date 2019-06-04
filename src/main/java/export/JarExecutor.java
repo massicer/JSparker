e006f3dcca5c379a2954f79c0e20a0a9603ceca2
Massimiliano Ceriani <massimilianoceriani08@gmail.com>
2019-06-04 18:20:44 +0200

    Inserted log prints inserted more example pipelines

--- src/main/java/export/JarExecutor.java
+++ src/main/java/export/JarExecutor.java
@@ -17,6 +17,7 @@ import org.json.JSONTokener;
 import parser.GrafterizerParser;
 import parser.pipeline.Pipeline;
 import pipenineExecutor.PipelineExecutor;
+import utility.LogManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -39,25 +40,40 @@ public class JarExecutor {
 	
 	public static void main (String[] args) throws Exception {
 		
+		LogManager.getShared().logInfo("JarExecutor - main - starting execution with "+args.length+" parameters");
+
 		// 0. Check if parameters are present
-		if(args.length < 2) throw new Exception("Jar executor Ex -  parameters missing ");
+		if(args.length < 2) {
+			LogManager.getShared().logError("JarExecutor - main - parameters must be >= 2");
+			throw new Exception("Jar executor Ex -  parameters missing - parameters must be >= 2");
+		}
 		
 		// 1.A get the file path
 		String inputFilePath = args[0];
+		LogManager.getShared().logInfo("JarExecutor - main - input data file path: "+inputFilePath);
 		
 		// 1.B init spark passing info
+		LogManager.getShared().logInfo("JarExecutor - main - preparing to init Spark session");
 		Dataset<Row> dataset = initSparkSession(inputFilePath);
+		LogManager.getShared().logSuccess("JarExecutor - main -  Spark session initialized");
 		
 		// 2. Get the json path
 		String jsonPath = args[1];
+		LogManager.getShared().logInfo("JarExecutor - main - pipeline file path: "+jsonPath);
+		LogManager.getShared().logInfo("JarExecutor - main - preparing to parse json");
 		JSONObject transf = readJsonInstruction(jsonPath);
+		LogManager.getShared().logSuccess("JarExecutor - main - json parsed");
 		
 		// 3. Parse instruction
+		LogManager.getShared().logInfo("JarExecutor - main - preparing to parse pipeline");
 		GrafterizerParser parser = new GrafterizerParser();
 		ArrayList<Pipeline>  pipelineParsed = parser.parsePipelineJson(transf);
+		LogManager.getShared().logSuccess("JarExecutor - main - pipeline parsed");
 		
 		// 4. Apply every instruction
+		LogManager.getShared().logInfo("JarExecutor - main - preparing to execute pipeline");
         Dataset<Row> result = PipelineExecutor.getShared().executePipeline(pipelineParsed, dataset);
+        LogManager.getShared().logSuccess("JarExecutor - main - pipeline executed");
         result.show();
 
 		
@@ -95,7 +111,6 @@ public class JarExecutor {
         SQLContext sqlContext = sparkSession.sqlContext();
         Dataset<Row> dataset = sqlContext.read()
                 .option("header", true)
-                //.csv("example-data.csv");
                 .csv(inputCsvPath); //comment option if you dont want an header
        
         return dataset;
